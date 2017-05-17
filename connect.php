54c27275f23581ed71ac104ce1afbe6863acd5d9
niaw <niawkung2@gmail.com>
2017-05-17 14:13:03 +0700

    fix

--- connect.php
+++ connect.php
@@ -8,13 +8,11 @@ $ftp_server = "water-pj.esy.es";
 $ftp_username = "u220965490.niawjunior";
 $ftp_userpass = "niaw2362537";
 $connect = mysqli_connect($host,$user,$pass,$db);
-$pdo = new PDO("mysql:dbname={$db};host={$host}", $user, $pass);
 $USER = $_SESSION["USER"];
 date_default_timezone_set('Asia/Bangkok');
 $time_log = time();
 $date = date("d-m-Y");
 $time = date("H:i:s");
 mysqli_query($connect,"UPDATE member SET time_log = '$time_log' WHERE user ='$USER' ");
-$pdo = new PDO("mysql:dbname={$db};host={$host}", $user, $pass);
 
 ?>
